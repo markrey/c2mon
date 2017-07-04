c238ab289443e947a6f2f78339d146eab5c83849
James Hamilton <james.hamilton@cern.ch>
2017-07-04 11:13:47 +0200

    Re-added @Component annotation.

--- c2mon-daq/c2mon-daq-core/src/main/java/cern/c2mon/daq/common/conf/core/EquipmentConfigurationFactory.java
+++ c2mon-daq/c2mon-daq-core/src/main/java/cern/c2mon/daq/common/conf/core/EquipmentConfigurationFactory.java
@@ -33,6 +33,7 @@ import cern.c2mon.shared.util.parser.SimpleXMLParser;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class EquipmentConfigurationFactory extends XMLTagValueExtractor implements ConfigurationXMLConstants {
 
   private static EquipmentConfigurationFactory theInstance;
