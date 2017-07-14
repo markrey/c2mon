617aba1796febc7a60bd3631d098fed781c76e85
James Hamilton <james.hamilton@cern.ch>
2017-07-14 11:17:08 +0200

    Removed unused tag service.

--- c2mon-server/c2mon-server-configuration/src/main/java/cern/c2mon/server/configuration/impl/ConfigurationLoaderImpl.java
+++ c2mon-server/c2mon-server-configuration/src/main/java/cern/c2mon/server/configuration/impl/ConfigurationLoaderImpl.java
@@ -129,8 +129,6 @@ public class ConfigurationLoaderImpl implements ConfigurationLoader {
 
   private final DeviceConfigHandler deviceConfigHandler;
 
-  private final TagLocationService tagLocationService;
-
   private final RuleTagCache ruleTagCache;
 
   private Environment environment;
@@ -177,7 +175,6 @@ public class ConfigurationLoaderImpl implements ConfigurationLoader {
                                  SequenceDAO sequenceDAO,
                                  ConfigurationProperties properties,
                                  ServerProperties serverProperties,
-                                 TagLocationService tagLocationService,
                                  RuleTagCache ruleTagCache) {
     super();
     this.processCommunicationManager = processCommunicationManager;
@@ -199,7 +196,6 @@ public class ConfigurationLoaderImpl implements ConfigurationLoader {
     this.sequenceDAO = sequenceDAO;
     this.daqConfigEnabled = properties.isDaqConfigEnabled();
     this.reportDirectory = serverProperties.getHome() + "/reports";
-    this.tagLocationService = tagLocationService;
     this.ruleTagCache = ruleTagCache;
   }
 
